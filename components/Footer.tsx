90d53fd26431d2aaf9d1184cdd2975bc7946e396
MATSUOKAAIRI <aMatsu20191121@gmail.com>
2025-04-26 00:02:49 +0900

    fix:Header,Footerの改善

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-      <footer className="bg-[#333] py-4 text-center text-[#ffd699] sticky bottom-0 w-full z-20">
-        <p>&copy; {new Date().getFullYear()} 非常食管理アプリ. All rights reserved.</p>
+      <footer className="bg-[#333] py-4 text-center text-[#ffd699] bottom-0 w-full z-20">
+        <p>&copy; {new Date().getFullYear()} 一備一会. All rights reserved.</p>
       </footer>
     );
   }
